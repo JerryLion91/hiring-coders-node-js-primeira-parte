c5f4f5c9ba8676cca67e979b8670dcf6fab1893b
Jessica Biali Gelman <bialigelman@gmail.com>
2021-07-22 16:45:36 -0300

    Aula 32 - Adicionando foto do usuario

--- src/router.js
+++ src/router.js
@@ -1,14 +1,24 @@
 import { Router } from "express";
+import Database from './database';
+import multer from 'multer';
+
 import UserController from "./app/controllers/UserController";
 import SessionController from './app/controllers/SessionController'
-import Database from './database';
 import authMiddleware from './app/middlewares/auth'
+import multerConfig from "./config/multer"
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
+
 // Rotas autenticadas
 routes.use(authMiddleware);
-routes.put("/users", UserController.update)
+routes.put("/users", UserController.update);
+
+// Upload de arquivos
+routes.post('/files', upload.single('file'), (req, res) =>{
+    return res.json({message: 'Tudo ok'});
+})
 export default routes;
